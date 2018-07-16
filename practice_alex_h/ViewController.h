4d502289271dd2c52496bd032aca45d394443523
alex_h <ygxhxw@163.com>
2018-07-16 11:29:26 +0800

    Initial Commit

---
+++ practice_alex_h/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  practice_alex_h
+//
+//  Created by bot on 2018/7/16.
+//  Copyright © 2018年 testDemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ practice_alex_h/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  practice_alex_h
+//
+//  Created by bot on 2018/7/16.
+//  Copyright © 2018年 testDemon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
